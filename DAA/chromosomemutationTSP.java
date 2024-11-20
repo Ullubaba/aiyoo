ddf588e5b2084c2073a2a3023e5680aced00b015
Ullubaba <ulluram67@gmail.com>
2024-11-20 22:07:38 +0530

    Update chromosomemutationTSP.java

--- DAA/chromosomemutationTSP.java
+++ DAA/chromosomemutationTSP.java
@@ -1,55 +1,90 @@
 import java.util.*;
 
-public class chromosomemutationTSP {
+public class TSPOptimalPath {
+
+    public static int calculatePathDistance(int[][] tspMatrix, List<Integer> chromosome) {
+        int totalDistance = 0;
+        int n = chromosome.size();
+
+        for (int i = 0; i < n - 1; i++) {
+            int from = chromosome.get(i);
+            int to = chromosome.get(i + 1);
+            int distance = tspMatrix[from][to];
+
+            if (distance == -1) return Integer.MAX_VALUE;
+            totalDistance += distance;
+        }
+
+        int lastCity = chromosome.get(n - 1);
+        int firstCity = chromosome.get(0);
+        int returnDistance = tspMatrix[lastCity][firstCity];
+        if (returnDistance == -1) return Integer.MAX_VALUE;
+        totalDistance += returnDistance;
+
+        return totalDistance;
+    }
+
+    public static List<Integer> mutateChromosome(List<Integer> chromosome) {
+        Random random = new Random();
+        int n = chromosome.size();
+
+        int idx1 = random.nextInt(n);
+        int idx2 = random.nextInt(n);
+
+        while (idx1 == idx2) {
+            idx2 = random.nextInt(n);
+        }
+
+        Collections.swap(chromosome, idx1, idx2);
+        return chromosome;
+    }
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        System.out.println("Enter the size of the matrix (m):");
-        int m = scanner.nextInt();
+        System.out.print("Enter the number of cities (n): ");
+        int n = scanner.nextInt();
 
-        int[][] matrix = new int[m][m];
-        System.out.println("Enter the matrix values (use -1 for infinity):");
-
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < m; j++) {
-                matrix[i][j] = scanner.nextInt();
+        int[][] tspMatrix = new int[n][n];
+        System.out.println("Enter the TSP matrix (-1 for infinity):");
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                tspMatrix[i][j] = scanner.nextInt();
             }
         }
-        System.out.println("Input Matrix:");
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < m; j++) {
-                if (matrix[i][j] == -1) {
-                    System.out.print("INF ");
-                } else {
-                    System.out.print(matrix[i][j] + " ");
-                }
-            }
-            System.out.println();
+
+        List<Integer> currentChromosome = new ArrayList<>();
+        for (int i = 0; i < n; i++) {
+            currentChromosome.add(i);
         }
 
-        List<Integer> chromosome = new ArrayList<>();
-        for (int i = 0; i < m; i++) {
-            chromosome.add(i);
-        }
+        List<Integer> bestChromosome = new ArrayList<>(currentChromosome);
+        int bestDistance = calculatePathDistance(tspMatrix, bestChromosome);
 
-        Collections.shuffle(chromosome);
+        System.out.println("Initial Path: " + bestChromosome);
+        System.out.println("Initial Distance: " + bestDistance);
 
-        System.out.println("Initial Chromosome (Random Tour): " + chromosome);
-        chromosome = mutate(chromosome);
-        System.out.println("Mutated Chromosome (After Mutation): " + chromosome);
-    }
-
-    public static List<Integer> mutate(List<Integer> chromosome) {
         Random random = new Random();
-        int size = chromosome.size();
-        int index1 = random.nextInt(size);
-        int index2 = random.nextInt(size);
+        int iterations = 10000;
 
-        while (index1 == index2) {
-            index2 = random.nextInt(size);
+        for (int i = 0; i < iterations; i++) {
+            List<Integer> mutatedChromosome = new ArrayList<>(currentChromosome);
+            mutateChromosome(mutatedChromosome);
+
+            int mutatedDistance = calculatePathDistance(tspMatrix, mutatedChromosome);
+
+            if (mutatedDistance < bestDistance) {
+                bestChromosome = new ArrayList<>(mutatedChromosome);
+                bestDistance = mutatedDistance;
+            }
+
+            if (mutatedDistance > bestDistance && random.nextDouble() < 0.01) {
+                bestChromosome = new ArrayList<>(mutatedChromosome);
+                bestDistance = mutatedDistance;
+            }
         }
 
-        Collections.swap(chromosome, index1, index2);
-        return chromosome;
+        System.out.println("Optimal Path: " + bestChromosome);
+        System.out.println("Optimal Distance: " + bestDistance);
     }
 }
